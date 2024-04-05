42a223109c819cb3249540be59f32774aa562bfd
ayushkumarOP <97456884+ayushkumarOP@users.noreply.github.com>
2024-04-05 23:31:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ from keras.models import load_model
 from streamlit_mic_recorder import mic_recorder
 import os
 
-# Load the pre-trained model
-model = load_model('classifier.h5')
+model = load_model('audio_classifier.h5')
 
 SAMPLE_RATE = 16000
 DURATION = 5
@@ -37,25 +36,24 @@ def process_audio(audio_file_path):
     return y_pred, audio
 
 def main():
-    st.title('Deepfake Audio Classifier')
-    st.write("Upload an audio file for classification.")
+    st.title('Deep:blue[Fake] Audio Classifier :sparkles:')
+    st.subheader('', divider='rainbow')
 
-    st.write("Record your voice, and play the recorded audio:")
+
+    st.subheader("Record the voice for DeepFake:")
     audio = mic_recorder(start_prompt="⏺️", stop_prompt="⏹️", key='recorder')
 
     if audio:
-        # Save the recorded audio to a temporary file
         temp_file_path = "temp_audio.flac"
         with open(temp_file_path, "wb") as temp_file:
             temp_file.write(audio['bytes'])
 
-        # Process the audio using the file path
         prediction, audio_clip = process_audio(temp_file_path)
 
-        # Clean up: Remove the temporary file
+        
         os.remove(temp_file_path)
 
-        # Display prediction
+
         col1, col2, col3 = st.columns(3)
         with col1:
             st.text(f"Prediction: {prediction[0][0]}")
@@ -68,7 +66,6 @@ def main():
             st.info("Your uploaded audio is below")
             st.audio(audio['bytes'])
 
-            # create a waveform
             fig = px.line(x=list(range(len(audio_clip))), y=audio_clip)
             fig.update_layout(
                 title="Waveform plot",
@@ -80,6 +77,34 @@ def main():
         with col3:
             st.info("Disclaimer")
             st.warning("These classification or detection mechanisms are not always accurate. They should be considered as a strong signal and not the ultimate decision makers.")
+    
+    st.subheader("Upload your Call Recording:")
+    uploaded_file = st.file_uploader("", type=['flac'])
+
+    if uploaded_file is not None:
+        prediction, audio_clip = process_audio(uploaded_file)
+
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            # st.text(prediction)
+            st.header("Result")
+            if prediction[0][0] == 1:
+                st.write('Prediction: Fake')
+            else:
+                st.write('Prediction: Real')
+
+        with col2:
+            st.header("Audio file")
+            st.info("Your uploaded audio is below")
+            st.audio(uploaded_file)
+
+            fig = px.line(x=list(range(len(audio_clip))), y=audio_clip)
+            fig.update_layout(
+                title="Waveform plot",
+                xaxis_title="Time",
+                yaxis_title="Amplitude"
+            )
+            st.plotly_chart(fig)
 
 if __name__ == '__main__':
     main()
